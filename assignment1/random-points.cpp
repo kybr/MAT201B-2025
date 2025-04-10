c0813ee8c546cb1b6d555170070ec1ddee9ee96b
Karl Yerkes <karl.yerkes@gmail.com>
2025-04-10 14:42:11 -0700

    in class push

--- assignment1/random-points.cpp
+++ assignment1/random-points.cpp
@@ -1,6 +1,4 @@
 #include "al/app/al_App.hpp"
-#include "al/graphics/al_Image.hpp" 
-#include "al/app/al_GUIDomain.hpp"
 #include "al/math/al_Random.hpp"
 using namespace al;
 #include <fstream> // for slurp()
@@ -10,74 +8,30 @@ Vec3f rvec() { return Vec3f(rnd::uniformS(), rnd::uniformS(), rnd::uniformS()); 
 RGB rcolor() { return RGB(rnd::uniform(), rnd::uniform(), rnd::uniform()); }
 
 std::string slurp(std::string fileName); // only a declaration
+
 class MyApp : public App {
-    Mesh grid, rgb, hsl, mine;
+    
     Mesh mesh;
     ShaderProgram shader;
-    Parameter pointSize{"pointSize", 0.004, 0.0005, 0.015};
-
-    void onInit() override {
-        auto GUIdomain = GUIDomain::enableGUI(defaultWindowDomain());
-        auto &gui = GUIdomain->newGUI();
-        gui.add(pointSize);  // add parameter to GUI
-    }
 
     void onCreate() override {
-        
-        auto image = Image("../colorful.png");
-        if (image.width() == 0) {
-            std::cout << "Image not found" << std::endl;
-            exit(1);
-        }
-
         mesh.primitive(Mesh::POINTS);
-
-        for (int y = 0; y < image.height(); ++y) {
-            for (int x = 0; x < image.width(); ++x) {
-                auto pixel = image.at(x, y);
-                mesh.vertex(float(x) / image.width(), float(y) / image.width(), 0);
-                mesh.color(pixel.r / 255.0, pixel.g / 255.0, pixel.b / 255.0);
-                mesh.texCoord(0.1, 0);
-
-
-                // fill in rgb, mine, and hsl meshes
-            }
+        for (int i = 0; i < 100; ++i) {
+            mesh.vertex(rvec());
+            mesh.color(rcolor());
+            mesh.texCoord(0.1, 0);
         }
-
-        nav().pos(0, 0, 5);
-
     
         if (! shader.compile(slurp("../point-vertex.glsl"), slurp("../point-fragment.glsl"), slurp("../point-geometry.glsl"))) {
             printf("Shader failed to compile\n");
             exit(1);
         }
-
-
-
     }
-
-    double time = 0;
-
-    void onAnimate(double dt) override {
-        time += dt;
-
-        if (time < 3) {
-
-        }
-
-
-    // clear and regenerate the mesh
-    // OR
-    // update mesh as a linear combination of other meshes
-    
-    // iterate over mesh, changing its vertices. 
-    }
-
 
     void onDraw(Graphics& g) override {
         g.clear(0.1);
         g.shader(shader);
-        g.shader().uniform("pointSize", pointSize);
+        g.shader().uniform("pointSize", 0.1);
         g.blending(true);
         g.blendTrans();
         g.depthTesting(true);
@@ -97,9 +51,6 @@ class MyApp : public App {
                 mesh.color(rcolor());
                 mesh.texCoord(0.1, 0);
             }
-        }
-
-        if (k.key() == '1') {
         }
 
         return true;
