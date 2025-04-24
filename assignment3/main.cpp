a3bffaaeafd4e403978f8d3c901596f1f7f438dc
Karl Yerkes <karl.yerkes@gmail.com>
2025-04-24 14:56:02 -0700

    in class

---
+++ assignment3/main.cpp
@@ -0,0 +1,117 @@
+#include "al/app/al_App.hpp"
+#include "al/app/al_GUIDomain.hpp"
+#include "al/graphics/al_Shapes.hpp"
+#include "al/math/al_Random.hpp"
+#include "al/math/al_Vec.hpp"
+
+using namespace al;
+
+#include <fstream>
+#include <vector>
+using namespace std;
+
+Vec3f randomVec3f(float scale) {
+  return Vec3f(rnd::uniformS(), rnd::uniformS(), rnd::uniformS()) * scale;
+}
+
+struct AlloApp : App {
+  Parameter timeStep{"/timeStep", "", 0.1, 0.01, 0.6};
+
+  Light light;
+  Material material;  // Necessary for specular highlights
+
+  Mesh mesh;
+
+  // size, color, species, sex, age, etc.
+  std::vector<Nav> agent;
+
+  void onInit() override {
+    auto GUIdomain = GUIDomain::enableGUI(defaultWindowDomain());
+    auto &gui = GUIdomain->newGUI();
+    gui.add(timeStep);
+  }
+
+  void onCreate() override {
+    nav().pos(0, 0, 10);
+    // addSphere(mesh);
+    // mesh.translate(0, 0, -0.1);
+    addCone(mesh);
+    mesh.scale(1, 1, 1.3);
+
+    mesh.generateNormals();
+    light.pos(0, 10, 10);
+
+    for (int i = 0; i < 10; ++i) {
+      Nav p;
+      p.pos() = randomVec3f(5);
+      p.quat()
+          .set(rnd::uniformS(), rnd::uniformS(), rnd::uniformS(),
+               rnd::uniformS())
+          .normalize();
+      // p.set(randomVec3f(5), randomVec3f(1));
+      agent.push_back(p);
+    }
+  }
+
+  Vec3f food;
+  double time = 0;
+  void onAnimate(double dt) override {
+    if (time > 7) {
+      time -= 7;
+
+      food = randomVec3f(15);
+      //
+      //
+    }
+    time += dt;
+
+    for (int i = 0; i < agent.size(); ++i) {
+      agent[i].moveF(5);
+      agent[i].faceToward(food, 0.03);
+    }
+
+    ///////
+    for (int i = 0; i < agent.size(); ++i) {
+      agent[i].step(dt);
+    }
+  }
+
+  bool onKeyDown(const Keyboard &k) override {
+    if (k.key() == '1') {
+    }
+  }
+
+  void onDraw(Graphics &g) override {
+    g.clear(0.27);
+    g.depthTesting(true);
+    g.lighting(true);
+    light.globalAmbient(RGB(0.1));
+    light.ambient(RGB(0));
+    light.diffuse(RGB(1, 1, 0.5));
+    g.light(light);
+    material.specular(light.diffuse() * 0.2);
+    material.shininess(50);
+    g.material(material);
+
+    for (int i = 0; i < agent.size(); ++i) {
+      g.pushMatrix();
+      g.translate(agent[i].pos());
+      g.rotate(agent[i].quat());
+      g.draw(mesh);
+      g.popMatrix();
+    }
+
+    Mesh m;
+    addSphere(m, 0.1);
+    m.generateNormals();
+    g.translate(food);
+    g.color(1, 0, 0);
+    g.draw(m);
+  }
+};
+
+int main() {
+  AlloApp app;
+  app.configureAudio(48000, 512, 2, 0);
+  app.start();
+}
