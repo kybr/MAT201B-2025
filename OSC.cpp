31ba33d42d7c235a1c42be1143f8de17ace57646
Karl Yerkes <karl.yerkes@gmail.com>
2025-05-15 17:41:10 -0700

    touch up OSC example

--- OSC.cpp
+++ OSC.cpp
@@ -2,20 +2,18 @@
 using namespace al;
 class MyApp : public App {
   ParameterColor background{"background"};
-  //Parameter background{"background", 1.0f, 0.0f, 1.0f};
   void onInit() override {
     parameterServer().verbose(true);
     parameterServer().registerParameter(background);
   }
 
-  // we don't want to add this message callback because it would
-  // mean the parameter server does not get the messages
-  // void onMessage(osc::Message& m) override {
-  //   std::cout << "Received message: " << m.addressPattern() << std::endl;
-  // }
+  void onMessage(osc::Message& m) override {
+    std::cout << "Received message: " << m.addressPattern() << std::endl;
+  }
 
   void onAnimate(double dt) override {
   }
   void onDraw(Graphics& g) override { g.clear(background); }
 };
+
 int main() { MyApp().start(); }
